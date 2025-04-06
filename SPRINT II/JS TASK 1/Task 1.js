4f81277c1e5e4cdd45f41781739ed0fdecdfa506
luxica-bency <146411037+luxica-bency@users.noreply.github.com>
2025-04-06 07:29:00 +0000

    JS task 1

---
+++ SPRINT II/JS TASK 1/Task 1.js
@@ -0,0 +1,73 @@
+console.log("Hello, World!");
+
+console.log("My name is Bency");
+
+console.log("My name is Bency");
+console.log(20);
+
+let word1="JavaScript";
+let word2="is progrmming language";
+let fullword=word1+" "+word2;
+console.log("fullword=",fullword);
+let num1=53;
+let num2=45;
+let sum=num1+num2;
+console.log("sum=",sum);
+let valid=true;
+let result= sum>=50;
+console.log(" The result=",result);
+
+let me="Bency";
+let age=20;
+console.log("My name is "+me+"and I am "+age+"years old" );
+
+console.log("My name is ",me);
+console.log("My age is ",age);
+
+let x=10;
+let y=20;
+console.log("addition=", x+y);
+console.log("subtraction=", x-y);
+console.log("multiplication=", x*y);
+console.log("division=", x/y); 
+
+console.log("The items in the array:")
+let pets=["Dog","Cat","Fish","Parrot","Rabbit"];
+for(let i=0; i<pets.length; i++){
+    console.log(pets[i]);
+}
+
+console.log("Printing a simple Pattern:");
+for(let i=1;i<=5;i++)
+{
+    console.log("*".repeat(i));
+}
+
+let name="Bency";
+let stage=20;
+let isworking=false;
+console.log("Name:",name);
+console.log("Age:",stage);   
+console.log("Is working:",isworking);
+
+name="Anu";
+age++;
+isworking=!isworking
+console.log("Name:",name);
+console.log("Age:",age);
+console.log("Is working:",isworking);
+
+name="Priya";
+age=25;
+dob="2000-01-01";
+height=5.8;
+weight=55.8;
+degree="B.A";
+gender="Felmale";
+console.log("Name:",name);
+console.log("Age:",age);   
+console.log("Date of Birth:",dob);
+console.log("Height:",height);  
+console.log("Weight:",weight);
+console.log("Degree:",degree);
+console.log("Gender=",gender);
